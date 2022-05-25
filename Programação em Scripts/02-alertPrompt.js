e70cc73ace8428a6189a3400b41d98fd792e99d8
Pedro Kielblock <p.kielblock123@gmail.com>
2022-05-25 13:10:12 -0300

    Delete Programação em Scripts directory

--- Programação em Scripts/02-alertPrompt.js
+++
@@ -1,9 +0,0 @@
-function alertBox(){
-    let x;
-    let age = prompt("Type your age: ");
-
-    if (age != null) {
-        x = "Age: " + age + " years old.";
-        document.getElementById("demo").innerHTML = x;
-    }
-}
--- Programação em Scripts/03-lineBreakAlert.js
+++
@@ -1,3 +0,0 @@
-function breakLineAlert() {
-    alert("That's an alert\nWith a line break.");
-}
