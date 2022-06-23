5b48507c548b0ba349b3e881695d7f7741852325
Pedro Kielblock <p.kielblock123@gmail.com>
2022-06-23 15:48:26 -0300

    php

---
+++ Programação em Scripts/PHP/39-exercicio32.php
@@ -0,0 +1,46 @@
+<?php
+
+function operation($numero1, $numero2, $operacao) {
+    switch ($operacao) {
+        case "+":
+            $resultado = $numero1 + $numero2;
+            break;
+        case "-":
+            $resultado = $numero1 - $numero2;
+            break;
+        case "*":
+            $resultado = $numero1 * $numero2;
+            break;
+        case "/":
+            $resultado = $numero1 / $numero2;
+            break;
+        default:
+            return false;
+            break;
+    }
+    return $resultado;
+}
+
+
+echo "4 e 2" . "<br>";
+echo "4 + 2 = " . operation(4, 2, "+") . "<br>";
+echo "4 - 2 = " . operation(4, 2, "-") . "<br>";
+echo "4 * 2 = " . operation(4, 2, "*") . "<br>";
+echo "4 / 2 = " . operation(4, 2, "/") . "<br>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    body {
+        font-size: 60px;
+    }
+</style>
+<body>
+</body>
+</html>
---
+++ Programação em Scripts/PHP/40-exercicio33.php
@@ -0,0 +1,27 @@
+<?php
+    function cortaTexto(&$texto, $corte) {
+        $inicio = substr($texto, 0, $corte);
+        $texto = substr($texto, $corte);
+        return $inicio;
+    }
+
+    $texto = "texto";
+    echo cortaTexto($texto, 2) . "<br>";
+    echo $texto;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    body {
+        font-size: 60px;
+    }
+</style>
+<body>
+</body>
+</html>
